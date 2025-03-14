0f16c78f2446b34753e17ee20c015fc791eada5e
Sachin Pandey <xachin300@gmail.com>
2025-03-14 11:37:13 -0500

    problem 6 using dict

---
+++ Unit 2/q7.py
@@ -0,0 +1,29 @@
+# Problem 6: Performances with Maximum Audience
+# You are given an array audiences consisting of positive integers representing 
+# the audience size for different performances at a music festival.
+
+# Return the combined audience size of all performances in audiences with the maximum audience size.
+
+# The audience size of a performance is the number of people who attended that performance.
+
+def max_audience_performances(audiences):
+    count_dict = {}
+    for count in audiences:
+        if count in count_dict:
+            count_dict[count] += 1
+        else:
+            count_dict[count] = 1
+    maxaud = max(count_dict.keys())
+    maxtotal = maxaud * count_dict[maxaud]
+    return maxtotal
+
+
+audiences1 = [100, 200, 200, 250, 150, 100, 250]
+audiences2 = [120, 180, 220, 150, 220]
+
+print(max_audience_performances(audiences1))
+print(max_audience_performances(audiences2))
+# Example Output:
+
+# 250
+# 440
