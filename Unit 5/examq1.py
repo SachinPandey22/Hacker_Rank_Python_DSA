d364716c6c724927b7c55569c2fd086a94c159a0
Sachin Pandey <xachin300@gmail.com>
2025-04-02 20:02:10 -0500

    Unit 5 Exam Soln

---
+++ Unit 5/examq1.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
