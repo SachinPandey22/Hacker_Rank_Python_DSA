06ac92aa2edaa42bc9932ebe10b3df9984c6c2e0
Sachin Pandey <xachin300@gmail.com>
2025-04-10 19:20:58 -0500

    Tree Problem 2 solved

---
+++ Unit 8/s1q3.py
@@ -0,0 +1,47 @@
+# Problem 3: Ivy Cutting
+# You have a trailing ivy plant represented by a binary tree. You want to take a cutting to start a new plant using the rightmost vine in the plant. Given the root of the plant, return a list with the value of each node in the path from the root node to the rightmost leaf node.
+
+# Evaluate the time complexity of your function. Define your variables and provide a rationale for why you believe your solution has the stated time complexity. Assume the input tree is balanced when calculating time complexity.
+
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def right_vine(root):
+    right_val = []
+    curr = root
+    while curr:
+        right_val.append(curr.val)
+        curr = curr.right
+    return right_val
+        
+  
+
+"""
+        Root
+      /      \
+    Node1    Node2
+  /         /    \
+Leaf1    Leaf2  Leaf3
+"""
+ivy1 = TreeNode("Root", 
+                TreeNode("Node1", TreeNode("Leaf1")),
+                TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")))
+
+"""
+      Root
+      /  
+    Node1
+    /
+  Leaf1  
+"""
+ivy2 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")))
+
+print(right_vine(ivy1))
+print(right_vine(ivy2))
+# Example Output:
+
+# ['Root', 'Node2', 'Leaf3']
+# ['Root']
