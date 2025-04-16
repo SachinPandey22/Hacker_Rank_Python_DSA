5648bb442777d2b2022645850a501ed83cde9a88
Sachin Pandey <xachin300@gmail.com>
2025-04-15 20:53:48 -0500

    unit 9 problem 2 solved

--- unit 9/q2.py
+++ unit 9/q2.py
@@ -1,3 +1,4 @@
+# Problem 2: Croquembouche - Print flavors in level order
 from collections import deque 
 
 # Tree Node class
@@ -7,76 +8,31 @@ class TreeNode:
         self.val = value
         self.left = left
         self.right = right
-
-def print_tree(root):
-    if not root:
-        return "Empty"
-    result = []
-    queue = deque([root])
-    while queue:
-        node = queue.popleft()
-        if node:
-            result.append(node.val)
-            queue.append(node.left)
-            queue.append(node.right)
-        else:
-            result.append(None)
-    while result and result[-1] is None:
-        result.pop()
-    print(result)
-
-def build_tree(values):
-    if not values:
-        return None
-
-    def get_key_value(item):
-        if isinstance(item, tuple):
-            return item[0], item[1]
-        else:
-            return None, item
-
-    key, value = get_key_value(values[0])
-    root = TreeNode(value, key)
-    queue = deque([root])
-    index = 1
-
-    while queue:
-        node = queue.popleft()
-        if index < len(values) and values[index] is not None:
-            left_key, left_value = get_key_value(values[index])
-            node.left = TreeNode(left_value, left_key)
-            queue.append(node.left)
-        index += 1
-        if index < len(values) and values[index] is not None:
-            right_key, right_value = get_key_value(values[index])
-            node.right = TreeNode(right_value, right_key)
-            queue.append(node.right)
-        index += 1
-
-    return root
+class Puff:
+    def __init__(self, flavor, left=None, right=None):
+        self.val = flavor
+        self.left = left
+        self.right = right
 
 def print_design(design):
     if not design:
-        return []  # if no puffs
-
-    queue = deque()  #  queue
-    queue.append(design)  # start with  top puff
-    result = []  # store the order of flavors
-
-    while queue:
-        current = queue.popleft()  # take the first puff in line
-        result.append(current.val)  # save its flavor
-
-        if current.left:  # if left puff exists
-            queue.append(current.left)
-
-        if current.right:  # if right puff exists
-            queue.append(current.right)
+        return []
     
-    croquembouche = Puff("Vanilla", 
+    queue = deque([design])
+    result = []
+    
+    while queue:
+        node = queue.popleft()
+        result.append(node.val)
+        
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    
+    print(result)
+croquembouche = Puff("Vanilla", 
                     Puff("Chocolate", Puff("Vanilla"), Puff("Matcha")), 
-                    
                     Puff("Strawberry"))
-    print_design(croquembouche)
+print_design(croquembouche)
 
- 
